5d86aaa494f9e14f863093d547dba82bc2f3e65b
Ben McMahen <bmcmahen@gmail.com>
2012-12-03 12:54:44 -0700

    fixed interval, playback

--- index.js
+++ index.js
@@ -1,60 +1,61 @@
 
 var Emitter = require('emitter');
+var event = require('event');
 
 module.exports = function(selector, options){
-	return new fadingCarousel(selector);
+	return new swapElements(selector);
 }
 
-var fadingCarousel = function(selector) {
+var swapElements = function(selector) {
 	Emitter.call(this);
 	this.list = document.querySelectorAll(selector);
 	this.length = this.list.length; 
 	this.currentIndex = 0; 
 	this.active = this.list[this.currentIndex];
+	this.active.className = this.active.className += ' visible';
 	this.duration = 7000; 
 }
 
-fadingCarousel.prototype = new Emitter; 
+swapElements.prototype = new Emitter; 
 
-fadingCarousel.prototype.play = function(duration){
+swapElements.prototype.play = function(duration){
 	if (this.timer)
 		return
 
-	duration = duration || self.duration; 
-	this.timer = window.setInterval(self.next(), duration);
+	duration = duration || this.duration; 
+	this.timer = setInterval(this.next.bind(this), duration);
 	this.emit('play');
 	return this; 
 };
 
-fadingCarousel.prototype.stop = function(){
-	window.clearInterval(this.timer);
+swapElements.prototype.stop = function(){
+	clearInterval(this.timer);
 	this.emit('stop');
 	return this; 
 }
 
-fadingCarousel.prototype.isFirst = function(){
+swapElements.prototype.isFirst = function(){
 	return this.currentIndex === 0; 
 }
 
-fadingCarousel.prototype.isLast = function(){
-	return this.current === this.total - 1; 
+swapElements.prototype.isLast = function(){
+	return this.currentIndex === this.length - 1; 
 }
 
-fadingCarousel.prototype.next = function(){
-	console.log('next called');
+swapElements.prototype.next = function(){
 	var nextIndex = this.isLast() ? 0 : this.currentIndex + 1;
 	this.emit('next');
 	this.goto(nextIndex);
 };
 
-fadingCarousel.prototype.prev = function(){
+swapElements.prototype.prev = function(){
 	var prev = this.isFirst() ? this.length - 1 : this.currentIndex - 1;
 	this.emit('prev');
 	this.goto(prev);
 	return this; 
 };
 
-fadingCarousel.prototype.goto = function(i) {
+swapElements.prototype.goto = function(i) {
 	this.active.className = this.active.className.replace( /(?:^|\s)visible(?!\S)/g , '' )
 	
 	var el = this.active = this.list[i];
